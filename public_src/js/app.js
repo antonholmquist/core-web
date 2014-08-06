6898333a370dc76283997656add380e4d8ee14d0
Anton Holmquist <anton.holmquist@gmail.com>
2014-08-06 18:41:16 +0200

    Managed to draw green triangle

--- public_src/js/app.js
+++ public_src/js/app.js
@@ -30,35 +30,20 @@ stage.addChild(bunny);
   
   var graphics = new PIXI.Graphics();
 
-      // set a fill and line style
-  graphics.beginFill(0xFF3300);
-
-  graphics.lineStyle(10, 0xffd900, 1);
-  
-  
-  // draw a shape
-  graphics.moveTo(50,50);
-  graphics.lineTo(250, 50);
-  graphics.lineTo(100, 100);
-  graphics.lineTo(250, 220);
-  graphics.lineTo(50, 220);
-  graphics.lineTo(50, 50);
-  graphics.endFill();
-
-  
-
 
     // begin a green fill..
-  graphics.beginFill(0xFFFFFF);
+  graphics.beginFill(0x00ff00);
    
   // draw a triangle using lines
   graphics.moveTo(0,0);
 
   graphics.lineTo(50, 100);
   graphics.lineTo(-50, 100);
+  graphics.lineTo(0,0);
    
   // end the fill
   graphics.endFill();
+  
 
 
   // add it the stage so we see it on our screens.. 
