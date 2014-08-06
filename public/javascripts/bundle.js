c4edbffe2f9b20b59cb48f7331ff2de51f5a8e97
Anton Holmquist <anton.holmquist@gmail.com>
2014-08-06 18:39:02 +0200

    Drawing something

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -14,6 +14,7 @@ document.body.appendChild(renderer.view);
 
 var stage = new PIXI.Stage;
 
+/*
 var bunnyTexture = PIXI.Texture.fromImage("bunny.png");
 var bunny = new PIXI.Sprite(bunnyTexture);
 
@@ -24,16 +25,61 @@ bunny.scale.x = 2;
 bunny.scale.y = 2;
 
 stage.addChild(bunny);
+*/
+
+{
+  
+  var graphics = new PIXI.Graphics();
+
+      // set a fill and line style
+  graphics.beginFill(0xFF3300);
+
+  graphics.lineStyle(10, 0xffd900, 1);
+  
+  
+  // draw a shape
+  graphics.moveTo(50,50);
+  graphics.lineTo(250, 50);
+  graphics.lineTo(100, 100);
+  graphics.lineTo(250, 220);
+  graphics.lineTo(50, 220);
+  graphics.lineTo(50, 50);
+  graphics.endFill();
+
+  
+
+
+    // begin a green fill..
+  graphics.beginFill(0xFFFFFF);
+   
+  // draw a triangle using lines
+  graphics.moveTo(0,0);
+
+  graphics.lineTo(50, 100);
+  graphics.lineTo(-50, 100);
+   
+  // end the fill
+  graphics.endFill();
+
+
+  // add it the stage so we see it on our screens.. 
+  stage.addChild(graphics);  
+
+}
+
+
 
 requestAnimationFrame(animate);
+
+
+
 
 // Animation loop
 function animate() {
 
+  renderer.render(stage);
 
-    renderer.render(stage);
-
-    requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
 }
 },{"./libs/pixi/pixi":2}],2:[function(require,module,exports){
 /**
