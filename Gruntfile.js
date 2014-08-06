17fc57e0f6fa724ffb2368d06602ded426112362
Anton Holmquist <anton.holmquist@gmail.com>
2014-08-06 17:21:22 +0200

    Add javascript watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,13 +21,21 @@ module.exports = function(grunt) {
         src: 'public_src/javascript/app.js',
         dest: 'public/javascript/bundle.js'
       }
+    },
+
+    watch: {
+      js: {
+        files: ['public_src/javascript/**/*.js'],
+        tasks: ['browserify']
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-express');
   grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['browserify:bundle', 'express:dev', 'express-keepalive']);
+  grunt.registerTask('default', ['browserify:bundle', 'watch', 'express:dev', 'express-keepalive']);
 
 
 } 
