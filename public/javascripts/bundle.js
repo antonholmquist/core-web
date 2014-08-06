4e518be8c9fe7323f4a7ca05949a62cf44c13747
Anton Holmquist <anton.holmquist@gmail.com>
2014-08-06 23:58:39 +0200

    Remove some old code

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -17,14 +17,6 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000 );
 camera.position.z = 1000;
 
-var geometry = new THREE.BoxGeometry( 200, 200, 200 );
-var material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
-
-
-var mesh = new THREE.Mesh( geometry, material );
-scene.add( mesh );
-
-
 var circleSegment1 = new CircleSegment()
 scene.add(circleSegment1.mesh );
 
@@ -36,29 +28,6 @@ var renderer = new THREE.WebGLRenderer({
 renderer.setSize( window.innerWidth, window.innerHeight );
 
 document.body.appendChild(renderer.domElement);
-
-
-/*
-var circleSegmentContainer = new PIXI.Graphics();
-circleSegmentContainer.scale.x = 100
-circleSegmentContainer.scale.y = 100
-circleSegmentContainer.position.x = width * 0.5
-circleSegmentContainer.position.y = height * 0.46
-stage.addChild(circleSegmentContainer)
-
-var circleSegment1 = new CircleSegment()
-circleSegmentContainer.addChild(circleSegment1.graphics)
-
-console.log("get local: " + JSON.stringify(circleSegment1.graphics.getLocalBounds()))
-console.log("get pivot: " + JSON.stringify(circleSegment1.graphics.pivot))
-*/
-
-/*
-var circleSegment2 = new CircleSegment()
-circleSegment2.graphics.x = 200
-stage.addChild(circleSegment2.graphics)
-*/
-
 
 requestAnimationFrame(animate);
 
@@ -74,8 +43,7 @@ function animate() {
   var dt = now - lastUpdate;
   lastUpdate = now;
 
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.02;
+  circleSegment1.mesh.rotation.z += 0.02;
 
   renderer.render( scene, camera );
   
